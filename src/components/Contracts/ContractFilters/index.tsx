93cf6001215a892ce10dc5a7a21d6fc0722b0175
klltx <kostya.svetashov.01@gmail.com>
2022-07-13 18:28:47 +0400

    create contract page

--- src/components/Contracts/ContractFilters/index.tsx
+++ src/components/Contracts/ContractFilters/index.tsx
@@ -5,17 +5,22 @@ import './ContractFilters.scss';
 import ContractDateFilter from './ContractDateFilter';
 import { Button } from '@mui/material';
 import NoteAddIcon from '@mui/icons-material/NoteAdd';
+import { useNavigate } from 'react-router-dom';
 
 interface IContractFilters { }
 
 const ContractFilters: FC<IContractFilters> = ({ ...props }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/create-contract')
+  }
 
   return (
     <div className='contract-filters'>
       <FullNameFilter />
       <CourseTitleFilter />
       <ContractDateFilter />
-      <Button variant="outlined" startIcon={<NoteAddIcon />}>
+      <Button variant="outlined" startIcon={<NoteAddIcon />} onClick={handleClick}>
         Добавить
       </Button>
     </div>
--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Contracts from '../Contracts';
+import CreateContractForm from '../CreateContractForm';
 import Login from '../Login';
 
 interface IRouter {
@@ -11,9 +12,10 @@ const Router: FC<IRouter> = ({ ...props }) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={'/'} element={<Navigate to={'/contracts'} />} />
+        <Route path={'/*'} element={<Navigate to={'/contracts'} />} />
         <Route path={'/login'} element={<Login />} />
         <Route path={'/contracts'} element={<Contracts />} />
+        <Route path={'/create-contract'} element={<CreateContractForm />} />
       </Routes>
 
     </BrowserRouter>
