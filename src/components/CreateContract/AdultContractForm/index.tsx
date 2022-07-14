472391aac85a252e14d4dc565af67aaab1e7d5e4
klltx <kostya.svetashov.01@gmail.com>
2022-07-14 20:09:24 +0400

    add validation to adult contract form

--- src/components/CreateContract/AdultContractForm/index.tsx
+++ src/components/CreateContract/AdultContractForm/index.tsx
@@ -8,14 +8,54 @@ import '../ContractForm.scss';
 interface IAdultContractForm { }
 
 const validationSchema = yup.object({
-  login: yup
+  surname: yup
     .string()
-    .min(4, 'Login should be of minimum 4 characters length')
-    .required('Login is required'),
-  password: yup
+    .required('Surname is required'),
+  firstName: yup
     .string()
-    .min(8, 'Password should be of minimum 8 characters length')
-    .required('Password is required'),
+    .required('Name is required'),
+  patronymic: yup
+    .string()
+    .required('Patronymic is required'),
+  address: yup
+    .string()
+    .required('Patronymic is required'),
+  birthday: yup
+    .string()
+    .required('Patronymic is required'),
+  position: yup
+    .string()
+    .required('Patronymic is required'),
+  job: yup
+    .string()
+    .required('Patronymic is required'),
+  passportSerial: yup
+    .string()
+    .required('Patronymic is required'),
+  passportNumber: yup
+    .string()
+    .required('Patronymic is required'),
+  passportIssue: yup
+    .string()
+    .required('Patronymic is required'),
+  snils: yup
+    .string()
+    .required('Patronymic is required'),
+  phone: yup
+    .string()
+    .required('Patronymic is required'),
+  email: yup
+    .string()
+    .required('Patronymic is required'),
+  course: yup
+    .string()
+    .required('Patronymic is required'),
+  contractDate: yup
+    .string()
+    .required('Patronymic is required'),
+  contractNumber: yup
+    .string()
+    .required('Patronymic is required'),
 });
 
 const textFieldStyle = { margin: '10px', width: '400px' };
@@ -24,8 +64,22 @@ const AdultContractForm: FC<IAdultContractForm> = ({ ...props }) => {
   const [isValid, setIsValid] = useState<boolean>(false);
   const formik = useFormik({
     initialValues: {
-      login: '',
-      password: '',
+      surname: '',
+      firstName: '',
+      patronymic: '',
+      address: '',
+      birthday: '',
+      position: '',
+      job: '',
+      passportSerial: '',
+      passportNumber: '',
+      passportIssue: '',
+      snils: '',
+      phone: '',
+      email: '',
+      course: '',
+      contractDate: '',
+      contractNumber: '',
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
@@ -40,183 +94,188 @@ const AdultContractForm: FC<IAdultContractForm> = ({ ...props }) => {
         <h3>Основная информация</h3>
         <TextField
           fullWidth
-          id="login"
-          name="login"
+          id="surname"
+          name="surname"
           label="Фамилия"
-          value={formik.values.login}
+          value={formik.values.surname}
           onChange={formik.handleChange}
-          error={formik.touched.login && Boolean(formik.errors.login)}
-          helperText={formik.touched.login && formik.errors.login}
+          error={formik.touched.surname && Boolean(formik.errors.surname)}
+          helperText={formik.touched.surname && formik.errors.surname}
           sx={textFieldStyle}
         />
         <TextField
           fullWidth
-          id="login"
-          name="login"
+          id="firstName"
+          name="firstName"
           label="Имя"
-          value={formik.values.login}
+          value={formik.values.firstName}
           onChange={formik.handleChange}
-          error={formik.touched.login && Boolean(formik.errors.login)}
-          helperText={formik.touched.login && formik.errors.login}
+          error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+          helperText={formik.touched.firstName && formik.errors.firstName}
           sx={textFieldStyle}
         />
         <TextField
           fullWidth
-          id="login"
-          name="login"
+          id="patronymic"
+          name="patronymic"
           label="Отчество"
-          value={formik.values.login}
+          value={formik.values.patronymic}
           onChange={formik.handleChange}
-          error={formik.touched.login && Boolean(formik.errors.login)}
-          helperText={formik.touched.login && formik.errors.login}
+          error={formik.touched.patronymic && Boolean(formik.errors.patronymic)}
+          helperText={formik.touched.patronymic && formik.errors.patronymic}
           sx={textFieldStyle}
         />
         <TextField
           fullWidth
-          id="login"
-          name="login"
+          id="address"
+          name="address"
           label="Адрес"
-          value={formik.values.login}
+          value={formik.values.address}
           onChange={formik.handleChange}
-          error={formik.touched.login && Boolean(formik.errors.login)}
-          helperText={formik.touched.login && formik.errors.login}
+          error={formik.touched.address && Boolean(formik.errors.address)}
+          helperText={formik.touched.address && formik.errors.address}
           sx={textFieldStyle}
         />
         <TextField
           fullWidth
-          id="login"
-          name="login"
+          id="birthday"
+          name="birthday"
           label="Дата рождения"
-          value={formik.values.login}
+          InputLabelProps={{ shrink: true, required: true }}
+          type={'date'}
+          value={formik.values.birthday}
           onChange={formik.handleChange}
-          error={formik.touched.login && Boolean(formik.errors.login)}
-          helperText={formik.touched.login && formik.errors.login}
+          error={formik.touched.birthday && Boolean(formik.errors.birthday)}
+          helperText={formik.touched.birthday && formik.errors.birthday}
           sx={textFieldStyle}
         />
         <h3>Образование и карьера</h3>
         <TextField
           fullWidth
-          id="login"
-          name="login"
+          id="position"
+          name="position"
           label="Должность"
-          value={formik.values.login}
+          value={formik.values.position}
           onChange={formik.handleChange}
-          error={formik.touched.login && Boolean(formik.errors.login)}
-          helperText={formik.touched.login && formik.errors.login}
+          error={formik.touched.position && Boolean(formik.errors.position)}
+          helperText={formik.touched.position && formik.errors.position}
           sx={textFieldStyle}
         />
         <TextField
           fullWidth
-          id="login"
-          name="login"
+          id="job"
+          name="job"
           label="Место работы"
-          value={formik.values.login}
+          value={formik.values.job}
           onChange={formik.handleChange}
-          error={formik.touched.login && Boolean(formik.errors.login)}
-          helperText={formik.touched.login && formik.errors.login}
+          error={formik.touched.job && Boolean(formik.errors.job)}
+          helperText={formik.touched.job && formik.errors.job}
           sx={textFieldStyle}
         />
         <h3>Паспортные данные</h3>
         <TextField
           fullWidth
-          id="login"
-          name="login"
+          id="passportSerial"
+          name="passportSerial"
           label="Серия"
-          value={formik.values.login}
+          value={formik.values.passportSerial}
           onChange={formik.handleChange}
-          error={formik.touched.login && Boolean(formik.errors.login)}
-          helperText={formik.touched.login && formik.errors.login}
+          error={formik.touched.passportSerial && Boolean(formik.errors.passportSerial)}
+          helperText={formik.touched.passportSerial && formik.errors.passportSerial}
           sx={textFieldStyle}
         />
         <TextField
           fullWidth
-          id="login"
-          name="login"
+          id="passportNumber"
+          name="passportNumber"
           label="Номер"
-          value={formik.values.login}
+          value={formik.values.passportNumber}
           onChange={formik.handleChange}
-          error={formik.touched.login && Boolean(formik.errors.login)}
-          helperText={formik.touched.login && formik.errors.login}
+          error={formik.touched.passportNumber && Boolean(formik.errors.passportNumber)}
+          helperText={formik.touched.passportNumber && formik.errors.passportNumber}
           sx={textFieldStyle}
         />
         <TextField
           fullWidth
-          id="login"
-          name="login"
+          id="passportIssue"
+          name="passportIssue"
           label="Кем и когда выдан"
-          value={formik.values.login}
+          value={formik.values.passportIssue}
           onChange={formik.handleChange}
-          error={formik.touched.login && Boolean(formik.errors.login)}
-          helperText={formik.touched.login && formik.errors.login}
+          error={formik.touched.passportIssue && Boolean(formik.errors.passportIssue)}
+          helperText={formik.touched.passportIssue && formik.errors.passportIssue}
           sx={textFieldStyle}
         />
         <TextField
           fullWidth
-          id="login"
-          name="login"
+          id="snils"
+          name="snils"
           label="СНИЛС"
-          value={formik.values.login}
+          value={formik.values.snils}
           onChange={formik.handleChange}
-          error={formik.touched.login && Boolean(formik.errors.login)}
-          helperText={formik.touched.login && formik.errors.login}
+          error={formik.touched.snils && Boolean(formik.errors.snils)}
+          helperText={formik.touched.snils && formik.errors.snils}
           sx={textFieldStyle}
         />
         <h3>Контактная информация</h3>
         <TextField
           fullWidth
-          id="login"
-          name="login"
+          id="phone"
+          name="phone"
           label="Номер телефона"
-          value={formik.values.login}
+          type={'tel'}
+          value={formik.values.phone}
           onChange={formik.handleChange}
-          error={formik.touched.login && Boolean(formik.errors.login)}
-          helperText={formik.touched.login && formik.errors.login}
+          error={formik.touched.phone && Boolean(formik.errors.phone)}
+          helperText={formik.touched.phone && formik.errors.phone}
           sx={textFieldStyle}
         />
         <TextField
           fullWidth
-          id="login"
-          name="login"
+          id="email"
+          name="email"
           label="Электронная почта"
-          value={formik.values.login}
+          type={'email'}
+          value={formik.values.email}
           onChange={formik.handleChange}
-          error={formik.touched.login && Boolean(formik.errors.login)}
-          helperText={formik.touched.login && formik.errors.login}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
           sx={textFieldStyle}
         />
         <h3>Курсы</h3>
         <TextField
           fullWidth
-          id="password"
-          name="password"
+          id="course"
+          name="course"
           label="Название курса"
-          type="password"
-          value={formik.values.password}
+          value={formik.values.course}
           onChange={formik.handleChange}
-          error={formik.touched.password && Boolean(formik.errors.password)}
-          helperText={formik.touched.password && formik.errors.password}
+          error={formik.touched.course && Boolean(formik.errors.course)}
+          helperText={formik.touched.course && formik.errors.course}
           sx={textFieldStyle}
         />
         <TextField
           fullWidth
-          id="login"
-          name="login"
+          id="contractDate"
+          name="contractDate"
           label="Дата заключения договора"
-          value={formik.values.login}
+          InputLabelProps={{ shrink: true, required: true }}
+          type={'date'}
+          value={formik.values.contractDate}
           onChange={formik.handleChange}
-          error={formik.touched.login && Boolean(formik.errors.login)}
-          helperText={formik.touched.login && formik.errors.login}
+          error={formik.touched.contractDate && Boolean(formik.errors.contractDate)}
+          helperText={formik.touched.contractDate && formik.errors.contractDate}
           sx={textFieldStyle}
         />
         <TextField
           fullWidth
-          id="login"
-          name="login"
+          id="contractNumber"
+          name="contractNumber"
           label="Номер договора"
-          value={formik.values.login}
+          value={formik.values.contractNumber}
           onChange={formik.handleChange}
-          error={formik.touched.login && Boolean(formik.errors.login)}
-          helperText={formik.touched.login && formik.errors.login}
+          error={formik.touched.contractNumber && Boolean(formik.errors.contractNumber)}
+          helperText={formik.touched.contractNumber && formik.errors.contractNumber}
           sx={textFieldStyle}
         />
         <Button color="primary" variant="contained" fullWidth type="submit">
